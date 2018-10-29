01710dea6628f4aac7b860c84eaed0d940208b67
ritu <rps57@njit.edu>
2018-10-28 23:39:48 -0400

    laravel server set up

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Home Page
+                    About
                 </div>
 
                 <div class="links">
