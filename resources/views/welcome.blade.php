d3ae896a26d86ab89d06e5a42a4917aa7eb4d9f4
ritu1985 <rps57@njit.edu>
2018-11-06 19:27:54 -0500

    New system commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,6 +85,7 @@
                 </div>
 
                 <div class="links">
+                    <a href="/about">About</a>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
